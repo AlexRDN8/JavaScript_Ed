c0ca798c67efbc7c7f6cb910cdad944e27c1ff45
AlexRDN8 <alexrodin8@bk.ru>
2023-04-07 16:15:03 +0300

    added task 100

---
+++ HM1_arrays/task100.js
@@ -0,0 +1,28 @@
+/*
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+*/
+/*
+const array = [1,-4,7,12];
+let value = array.reduce(function(sum, current) {
+    if (current > 0){
+        sum += current;
+    }
+    return sum;
+  }, 0);
+console.log(value);
+*/
+const array = [1,2,3,4,5];
+function positiveSum(arr) {
+    let result = arr.reduce(function(sum, current) {
+      if (current > 0){
+          sum += current;
+      }
+      return sum;
+    }, 0);
+    return result;
+  }
+  console.log(positiveSum(array));
