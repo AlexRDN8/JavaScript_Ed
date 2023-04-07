144c6cae3bd61d653acaf7dc939a744797a79d89
AlexRDN8 <alexrodin8@bk.ru>
2023-04-07 16:55:00 +0300

    added task102

--- HM1_arrays/task100.js
+++ HM1_arrays/task100.js
@@ -5,16 +5,8 @@ Example [1,-4,7,12] => 1 + 7 + 12 = 20
 
 Note: if there is nothing to sum, the sum is default to 0.
 */
-/*
-const array = [1,-4,7,12];
-let value = array.reduce(function(sum, current) {
-    if (current > 0){
-        sum += current;
-    }
-    return sum;
-  }, 0);
-console.log(value);
-*/
+
+
 const array = [1,2,3,4,5];
 function positiveSum(arr) {
     let result = arr.reduce(function(sum, current) {
