f9fd0529fc276d76f525d139d45de71acdef4859
AlexRDN8 <alexrodin8@bk.ru>
2023-04-10 21:05:35 +0300

    added task114

---
+++ HM1_arrays/task114.js
@@ -0,0 +1,23 @@
+/*
+Write Number in Expanded Form
+You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+expandedForm(12); // Should return '10 + 2'
+expandedForm(42); // Should return '40 + 2'
+expandedForm(70304); // Should return '70000 + 300 + 4'
+NOTE: All numbers will be whole numbers greater than 0.
+*/
+function expandedForm(num) {
+    let array = String(num).split("");
+    let newArray = [];
+    array = array.map(function(element,index){
+        return element * Math.pow(10, array.length - index - 1);
+    })
+    for (const element of array) {
+      if (element !== 0){
+        newArray.push(element)
+      }
+    }
+    return newArray.join(" + "); 
+  }
+  console.log(expandedForm(70304))
