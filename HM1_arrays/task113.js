4616b99d132f96aada9c0820436ec6e2b1dbede5
AlexRDN8 <alexrodin8@bk.ru>
2023-04-10 17:47:00 +0300

    -

---
+++ HM1_arrays/task113.js
@@ -0,0 +1,25 @@
+/*
+Let's create some scrolling text!
+
+Your task is to complete the function which takes a string, and returns an array with 
+all possible rotations of the given string, in uppercase.
+
+Example
+scrollingText("codewars") should return:
+
+[ "CODEWARS",
+  "ODEWARSC",
+  "DEWARSCO",
+  "EWARSCOD",
+  "WARSCODE",
+  "ARSCODEW"
+  "RSCODEWA",
+  "SCODEWAR" ]
+*/
+function scrollingText(text){
+  newArray = [];
+  const array = text.toUpperCase().split("");
+ 
+}
+//!!! Не смог решить. 
+console.log(scrollingText("codewars"));
