74f6c76cb2d8f0436c5cb83d882fa92ea87a23bd
AlexRDN8 <alexrodin8@bk.ru>
2023-04-11 14:12:56 +0300

    fixed task400

--- HM0/task400.js
+++ HM0/task400.js
@@ -11,19 +11,12 @@ Your function would return the following array:
 All of the strings in the array passed to your function will be different lengths, 
 so you will not have to decide how to order multiple strings of the same length.
 */
-let array = ["Telescopes", "Glasses", "Eyes", "Monocles"];
-function sortArray(arr){
-    for (let i = 0; i <arr.length; i++){
-        let min = i;
-        for (let j = i+1; j < arr.length; j++){ 
-            if (arr[j].length < arr[min].length){
-                min = j;                 
-            }
-        }
-        let temp = arr[min];
-        arr[min] = arr[i];
-        arr[ i ] = temp;        
+
+function sortByLength (array){
+    function compare(a, b) {
+        return a.length - b.length;
     }
+    const arr = array.sort(compare);        
     return arr;
 }
-console.log (sortArray(array));
+console.log (sortByLength(["Telescopes", "Glasses", "Eyes", "Monocles"]));
