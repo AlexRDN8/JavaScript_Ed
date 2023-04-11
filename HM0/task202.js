fbb5ba76d06ab56625fb1bc7864fee0069202004
AlexRDN8 <alexrodin8@bk.ru>
2023-04-11 13:47:03 +0300

    fixed task202

--- HM0/task202.js
+++ HM0/task202.js
@@ -14,20 +14,22 @@ When an array in the array is null or empty, the method should return 0 too!
 There will always be a missing element and its length will be always between the given arrays.
 */
 
-const arr = [[1, 2], [4, 5, 1, 1], [1], [5, 6, 7, 8, 9]];
 
-function findLengthOfMissingArr(array){
-    array.sort();
+function getLengthOfMissingArray(arrayOfArrays){
+    function compare(a, b) {
+        return a.length - b.length;
+      }
+    const arr = arrayOfArrays.sort(compare);
     if (arr.length == 0){
         return 0;
     }
     for (let i = 1; i < arr.length; i++){
-    if (arr[i - 1].length == 0){
-        return 0;
-    }    
-    if (arr[i].length - arr[i - 1].length != 1){
-        return arr[i].length - 1;
-    }   
+        if (arr[i - 1].length == 0){
+            return 0;
+        }    
+        else if (arr[i].length - arr[i - 1].length != 1){
+            return arr[i].length - 1;
+        }   
     }
 }
-console.log(findLengthOfMissingArr(arr));
+console.log(getLengthOfMissingArray([ [ 5, 2, 9 ], [ 4, 5, 1, 1 ], [ 1 ], [ 5, 6, 7, 8, 9 ]]));
