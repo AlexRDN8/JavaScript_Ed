4156c9629661da382a1ec152116ee8bc85e9d9f7
AlexRDN8 <alexrodin8@bk.ru>
2023-04-18 18:47:38 +0300

    added task 206

--- HM_arrays2/task206.js
+++ HM_arrays2/task206.js
@@ -1,44 +1,30 @@
 /*
-You are going to be given an array of integers. Your job is to take that array and find an index N where the sum of the integers to the left of N is equal to the sum of the integers to the right of N. If there is no index that would make this happen, return -1.
+Given an integer n, find the maximal number you can obtain by deleting exactly one digit of the given number.
 
-For example:
+Example
+For n = 152, the output should be 52;
 
-Let's say you are given the array {1,2,3,4,3,2,1}:
-Your function will return the index 3, because at the 3rd position of the array, the sum of left side of the index ({1,2,3}) and the sum of the right side of the index ({3,2,1}) both equal 6.
+For n = 1001, the output should be 101.
 
-Let's look at another one.
-You are given the array {1,100,50,-51,1,1}:
-Your function will return the index 1, because at the 1st position of the array, the sum of left side of the index ({1}) and the sum of the right side of the index ({50,-51,1,1}) both equal 1.
+Input/Output
+[input] integer n
 
-Last one:
-You are given the array {20,10,-80,10,10,15,35}
-At index 0 the left side is {}
-The right side is {10,-80,10,10,15,35}
-They both are equal to 0 when added. (Empty arrays are equal to 0 in this problem)
-Index 0 is the place where the left side and right side are equal.
+Constraints: 10 ≤ n ≤ 1000000.
 
-Note: Please remember that in most programming/scripting languages the index of an array starts at 0.
-
-Input:
-An integer array of length 0 < arr < 1000. The numbers in the array can be any integer positive or negative.
-
-Output:
-The lowest index N where the side to the left of N is equal to the side to the right of N. If you do not find an index that fits these rules, then you will return -1.
-
-Note:
-If you are given an array with multiple answers, return the lowest correct index.
+[output] an integer
 */
-function findEvenIndex(arr)
-{
-    for (let i = 0; i < arr.length; i++) {
-        const leftSide = arr.slice(0, i);
-        const rightSide = arr.slice(i+1);
-        const sumOfLeft = leftSide.reduce((sum, current)=> sum + current , 0);
-        const sumOfright = rightSide.reduce((sum, current)=> sum + current, 0);
-        if(sumOfLeft === sumOfright){
-            return i
-        }    
+function deleteDigit(n) {
+    const array = [...'' + n].map(Number) //уточнить
+    const newArray = [];
+    const sortingFunction = (a , b) => a - b;
+
+    for (let i = 0; i < array.length; i++) {
+        let copyOfArray = array.slice();
+        copyOfArray.splice(i , 1)
+        newArray.push(Number(copyOfArray.join("")))   
     }
-    return -1;
+    const sortedArray = newArray.sort(sortingFunction);
+    return sortedArray.at(-1);
 }
-console.log(findEvenIndex([20,10,30,10,10,15,35]));
+// проверочное число n = 597306, 809053.  
+console.log(deleteDigit([809053]));
